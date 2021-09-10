e7ec015b220a359a28e8d4ccc991dff67b029ad7
IsiTapier <41296026+IsiTapier@users.noreply.github.com>
2021-09-10 12:28:48 +0200

    sensor code

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,159 @@
 #include <Arduino.h>
 #include <Adafruit_VL53L0X.h>
 #include <array>
+#include <map>
 
-#define LIDAR_AVERAGE 100
-#define LIDAR_VALUEPERCENTAGE 0.5
+//pins
+#define LIDAR_SDA 18
+#define LIDAR_SCL 19
+#define IR_ANALOG 34
 
+//settings
+#define LIDAR false
+#define IR true
+//LIDAR
+#define LIDAR_SAMPLES 1000
+#define LIDAR_VALUE_CORRECTION -30
+//IR
+#define IR_SAMPLES 10000
+#define IR_CYCLES 10
+//IR mapping
+#define A0 +9.42450609268053
+#define A1 +1.76515427480637
+#define A2 -5.03610159944910
+#define A3 +6.88283703313403
+#define A4 -5.02366498969399
+#define A5 +2.08076080866994
+#define A6 -4.87776360389973
+#define A7 +5.99683334169558
+#define A8 -2.97424332815192
+#define E0 +0
+#define E1 -1
+#define E2 -4
+#define E3 -7
+#define E4 -10
+#define E5 -13
+#define E6 -17
+#define E7 -21
+#define E8 -25
+#define GRADE 8
+#define PRINT_PARTS false
+
+//macros
+#define DEBUG debug&&Serial
+#define A(n) (n==0?A0:n==1?A1:n==2?A2:n==3?A3:n==4?A4:n==5?A5:n==6?A6:n==7?A7:n==8?A8:0)
+#define E(n) (n==0?E0:n==1?E1:n==2?E2:n==3?E3:n==4?E4:n==5?E5:n==6?E6:n==7?E7:n==8?E8:0)
+#define PART(n, x) (A(n)*pow(10, E(n))*pow(x, n))
+
+//instances
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-std::array<uint16_t, LIDAR_AVERAGE> values;
+uint16_t values[LIDAR_SAMPLES];
+std::map<short, short> amounts;
 
-void setup() {
-  Serial.begin(115200);
-
-  // wait until serial port opens for native USB devices
-  while (! Serial) {
-    delay(1);
-  }
-  Wire.begin(18, 19);
-  Serial.println("Adafruit VL53L0X test");
+void lidarInit(bool force = false, bool debug = true) {
+  if(DEBUG)
+    Serial.println(F("Adafruit VL53L0X start init"));
+  Wire.begin(LIDAR_SDA, LIDAR_SCL);
   if (!lox.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
+    if(DEBUG) {
+      Serial.println(F("Failed to boot VL53L0X"));
+      Serial.print(F("retry: ")); Serial.println(F(force?"true":"false"));
+    }
+    while(force&&!lox.begin());
   }
-  // power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+  if(DEBUG) {
+    Serial.print(F("Adafruit VL53L0X init ")); Serial.println(F(lox.Status!=-5?"successfull":"failed"));
+  }
 }
+   
 
-
-void loop() {
+double getLidarDistance(bool debug = true) { //Average
   VL53L0X_RangingMeasurementData_t measure;
-  short count = LIDAR_AVERAGE;
+  short count = LIDAR_SAMPLES;
   
-  Serial.print("Reading a measurements... ");
-  
-  for(int i = 0; i < LIDAR_AVERAGE; i++) {
+  //get data
+  for(int i = 0; i < LIDAR_SAMPLES; i++) {
     lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-
     if (measure.RangeStatus != 4) {  // phase failures have incorrect data
       values[i] = measure.RangeMilliMeter;
     } else {
       count--;
-      Serial.println(" out of range ");
-    }a
+      if(DEBUG)
+        Serial.println(F("WARNING: lidar out of range"));
+    }
   }
 
-  std::sort(std::begin(values), std::end(values));  //sorting
-  short mid = floor(count/2);
-  short range = mid*LIDAR_VALUEPERCENTAGE;
-  short low = mid - range - count%2==0?1:0;
-  short high = mid + range;
+  //average
   double value = 0;
-  for(int i = low; i <= high; i++)
-    value+=values.at(i);
-  Serial.print(values.at(mid));
-  value/=high-low+1;
-  Serial.print("Distance (mm): "); Serial.println(value);
+  for(int i = 0; i < count; i++)
+    value+=values[i];
+  value/=count;
 
+  //correct value
+  value+=LIDAR_VALUE_CORRECTION;
+
+  return value;
+}
+
+void irInit() {
+  pinMode(IR_ANALOG, INPUT);
+}
+
+short getIrValue() { //modal wert
+  amounts.clear();
+  for(int i = 0; i < IR_SAMPLES; i++)
+    amounts[analogRead(IR_ANALOG)]++;
+  short value = 0;
+  for(auto const& x : amounts)
+    if(x.second > amounts[value])
+      value = x.first;
+  return value;
+}
+
+short getIrDistanceRaw() {
+  amounts.clear();
+  for(int i = 0; i < IR_CYCLES; i++)
+    amounts[getIrValue()]++;
+  short value = 0;
+  for(auto const& x : amounts)
+    if(x.second > amounts[value])
+      value = x.first;
+  return value;
+}
+
+short getIrDistance(bool debug = false) {
+  int raw = getIrDistanceRaw();
+  if(DEBUG) {
+    Serial.print(raw);
+    Serial.print("\t");
+  }
+  double value = 0;
+  for(int i = 0; i <= GRADE; i++) {
+    double part = PART(i, raw);
+    if(DEBUG && PRINT_PARTS) {
+      Serial.print(part);
+      Serial.print("\t");
+    }
+    value+=part;
+  }
+  if(DEBUG)
+    Serial.println(value);
+  return value;
+}
+
+void setup() {
+  Serial.begin(115200);
+  // wait until serial port opens for native USB devices
+  while (!Serial)
+    delay(1);
+  Serial.println();
+  if(LIDAR) lidarInit();
+  if(IR) irInit();
+}
+
+void loop() {
+  Serial.print(millis());
+  Serial.print("\t");
+  if(LIDAR) Serial.println(getLidarDistance());
+  if(IR) getIrDistance(true);
 }
