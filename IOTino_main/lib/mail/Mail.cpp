5feaa4eccbf1877f8ffc57e36c96aa5a19738ef3
Jones Braun <47891728+JonasBraun04@users.noreply.github.com>
2021-12-06 09:57:33 +0100

    ESP Mail 1 test

---
+++ IOTino_main/lib/mail/Mail.cpp
@@ -0,0 +1,116 @@
+#include "Mail.h"
+
+
+SMTPSession smtp;
+
+ //https://myaccount.google.com/lesssecureapps?pli=1
+/* The SMTP Session object used for Email sending */
+
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+
+String Mail::sendMail(int value){
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to AP");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /** Enable the debug via Serial port
+   * none debug or 0
+   * basic debug or 1
+  */
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+  /* Declare the session config data */
+  ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = "IoTino";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "Papierstatus:" + value;
+  message.addRecipient("Hausmeister", RECIPIENT_EMAIL);
+
+  
+/*
+  //Send HTML message
+  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Hello World!</h1><p>- Sent from ESP board</p></div>";
+  message.html.content = htmlMsg.c_str();
+  message.html.content = htmlMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+  
+  */
+  //Send raw text message
+  String textMsg = "Papier ha teinen Füllstand von:" + value;
+  message.text.content = textMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  /* Set the custom message header */
+  //message.addHeader("Message-ID: <abcde.fghij@gmail.com>");
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&session))
+    return "Mail sent";
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+    return "Error sending Mail";
+}
+
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
+    }
+    Serial.println("----------------\n");
+  }
+}
---
+++ IOTino_main/lib/mail/Mail.h
@@ -0,0 +1,30 @@
+#ifndef Mail_h
+#define Mail_h
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESP_Mail_Client.h>
+
+
+#define WIFI_SSID "JCBS-Schüler"
+#define WIFI_PASSWORD "K1,14DWwFuwuu."
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465
+
+/* The sign in credentials */
+#define AUTHOR_EMAIL "isajah.tappe@gmail.com"
+#define AUTHOR_PASSWORD "qwertz777"
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "jonas.na.braun@web.de"
+#define RECIPIENT_EMAIL_TWO "hausmeister@jcbs.de"
+
+
+class Mail {
+    public:
+    
+    static String sendMail(int value);
+    private:
+    
+};
+
+#endif
